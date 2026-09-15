3368ed18ed1367d94fbba36bd8df154af39374a3
Nuno do Carmo <nuno.carmo@suse.com>
2026-09-15 09:33:26 +0200

    Fix Hyprland 0.56 schema keys in looknfeel.lua

--- system_files/etc/skel/.config/hypr/looknfeel.lua
+++ system_files/etc/skel/.config/hypr/looknfeel.lua
@@ -25,14 +25,13 @@ hl.config({
   },
 
   misc = {
-    vfr = true,
     vrr = 0,
     disable_hyprland_logo = true,
     disable_splash_rendering = true,
+    force_default_wallpaper = 0,
   },
 
   render = {
     direct_scanout = false,
-    explicit_sync = 0,
   },
 })
